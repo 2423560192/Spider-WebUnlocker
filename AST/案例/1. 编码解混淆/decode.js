c412ca5c0b17c33ef75bdb24e7cb3ad59508786e
jiaoxingk <2480419172@qq.com>
2025-05-21 11:29:06 +0800

    AST练习-编码解混淆

---
+++ AST/案例/1. 编码解混淆/decode.js
@@ -0,0 +1,9 @@
+Ze[$_DADR(91)] = {
+  "$_BJET": function (e, t) {
+    var $_CECDo = PaLDJ.$_CS,
+      $_CECC_ = ["$_CECGP"].concat($_CECDo),
+      $_CECEX = $_CECC_[1];
+    $_CECC_.shift();
+    var $_CECFB = $_CECC_[0];
+  }
+};
---
+++ AST/案例/1. 编码解混淆/encode.js
@@ -0,0 +1,9 @@
+Ze[$_DADR(91)] = {
+    "\u0024\u005f\u0042\u004a\u0045\u0054": function (e, t) {
+        var $_CECDo = PaLDJ.$_CS
+            , $_CECC_ = ['$_CECGP'].concat($_CECDo)
+            , $_CECEX = $_CECC_[1];
+        $_CECC_.shift();
+        var $_CECFB = $_CECC_[0];
+    }
+}
